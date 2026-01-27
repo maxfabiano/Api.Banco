10af6963d89db5e6126bfae5fa0a9167d13c3bba
maxfabiano <64711484+maxfabiano@users.noreply.github.com>
2026-01-27 13:21:08 -0300

    LogsModel

---
+++ Api.Banco.Database.Tarifas/LogsModel/AuditInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Banco.Database.Tarifas.Logs
+{
+    public class AuditInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            var entries = eventData.Context.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified || e.State == EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                
+                var entityName = entry.Entity.GetType().Name;
+                var state = entry.State.ToString();
+
+                Console.WriteLine($"[AUDIT LOG] {DateTime.Now}: Entidade {entityName} alterada para {state}");
+
+                foreach (var prop in entry.Properties)
+                {
+                    if (prop.IsModified)
+                    {
+                        Console.WriteLine($"  - Propriedade {prop.Metadata.Name}: '{prop.OriginalValue}' -> '{prop.CurrentValue}'");
+                    }
+                }
+            }
+            return base.SavingChanges(eventData, result);
+        }
+    }
+}
